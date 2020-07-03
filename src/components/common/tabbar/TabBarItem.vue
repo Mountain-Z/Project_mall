af8f8323fb07f33811c07ca4b92a7cf01e27a9c2
Mountain-Z <you@example.com>
2020-07-03 12:13:28 +0800

    gai

--- src/components/common/tabbar/TabBarItem.vue
+++ src/components/common/tabbar/TabBarItem.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="item" @click="itemClick">
-    <div v-show="!isActive"><slot name="icon"></slot></div>
-    <div v-show="isActive"><slot name="active-icon"></slot></div>
-    <div><slot name="text"></slot></div>
+    <div v-show="isActive">
+      <slot name="icon"></slot>
+    </div>
+    <div v-show="!isActive">
+      <slot name="active-icon"></slot>
+    </div>
+    <div :class="{'item-text':true,'Active':!isActive}">
+      <slot name="text"></slot>
+    </div>
   </div>
 </template>
 
@@ -15,7 +21,7 @@ export default {
   },
   computed: {
     isActive() {
-      return this.$route.path.indexOf(this.link) !== -1;
+      return this.$route.path.indexOf(this.link) == -1;
     }
   },
   methods: {
@@ -34,5 +40,13 @@ export default {
 #item img {
   height: 24px;
   width: 24px;
+}
+.item-text {
+  font-size: 12px;
+  margin-top: 3px;
+  color: #333;
+}
+.Active {
+  color: red;
 }
 </style>
